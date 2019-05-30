ec1df70232af4b11a3d69221fbf1034e68d08a86
CosmicDan <dconnolly.au@gmail.com>
2019-05-30 22:41:27 +1000

    Add Build.bat

---
+++ Build.bat
@@ -0,0 +1,16 @@
+@echo off
+CD "%~dp0"
+echo.
+echo ######
+echo # haxe Swivel.hxml
+echo ######
+haxe.exe Swivel.hxml
+echo.
+echo ######
+echo # call PackageApp.bat
+echo ######
+call PackageApp.bat
+echo.
+
+:END
+pause
